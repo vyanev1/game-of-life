6b08191d998025384e9d406e2fa229f8711f0c0e
Victor Yanev <56009006+vyanev1@users.noreply.github.com>
2020-07-31 21:20:21 +0300

    Initial commit

--- game.py
+++ game.py
@@ -17,28 +17,3 @@ class Game:
         if color == 'green':
             self.targetCount += 1
         self.toNextGeneration()
-
-
-if __name__ == '__main__':
-    # grid size
-    sizeX, sizeY = input().strip().split(',')
-    sizeX = int(sizeX)
-    sizeY = int(sizeY)
-
-    # initial state input (generation zero)
-    inputMatrix = []
-    for _ in range(sizeY):
-        inputMatrix.append([int(cell) for cell in input().strip()])
-
-    # get target cell coordinates and number of generations that will be played
-    targetX, targetY, N = input().strip().split(',')
-    targetX = int(targetX)
-    targetY = int(targetY)
-    N = int(N)
-
-    # Initiate the game and play for N generations
-    game = Game(sizeX, sizeY, inputMatrix)
-    while game.generation <= N:
-        game.updateState(targetX, targetY)
-
-    print(game.targetCount)
